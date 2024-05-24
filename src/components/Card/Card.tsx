9c4a9d745b98965e07552b5eeb3f41b00b90e4be
Hugo Masclet <hugo@lukso.io>
2024-05-24 19:18:38 +0200

    Fix tooltip html

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -21,16 +21,16 @@ const Card: React.FC<Props> = ({ title, value, info }) => {
           <div className="hs-tooltip">
             <div className="hs-tooltip-toggle">
               <svg
-                className="flex-shrink-0 size-4 text-gray-500 dark:text-neutral-500"
+                className="flex-shrink-0 size-4 text-gray-500"
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
                 height="24"
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               >
                 <circle cx="12" cy="12" r="10" />
                 <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3" />
@@ -40,6 +40,7 @@ const Card: React.FC<Props> = ({ title, value, info }) => {
                 className="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 bg-gray-900 text-xs font-medium text-white rounded shadow-sm dark:bg-neutral-700"
                 role="tooltip"
               >
+                {info}
               </span>
             </div>
           </div>
