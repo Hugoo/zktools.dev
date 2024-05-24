5eab0b4c8559d16f098fa81d972d71f303e0a3de
Hugo Masclet <hugo@lukso.io>
2024-05-24 19:21:36 +0200

    Add Verify page

---
+++ src/app/verify/page.tsx
@@ -0,0 +1,7 @@
+export default function Verify() {
+  return (
+    <div>
+      <h1>Verify</h1>
+    </div>
+  );
+}
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -52,7 +52,12 @@ const NavBar: React.FC = () => {
           className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow sm:block"
         >
           <div className="flex flex-col gap-y-4 gap-x-0 mt-5 sm:flex-row sm:items-center sm:justify-end sm:gap-y-0 sm:gap-x-7 sm:mt-0 sm:ps-7">
-          
+            <Link
+              href="/verify"
+              className="font-medium text-gray-500 hover:text-gray-400 sm:py-6"
+            >
+              Verify
+            </Link>
             <Link
               href="/r1cs"
               className="font-medium text-gray-500 hover:text-gray-400 sm:py-6"
