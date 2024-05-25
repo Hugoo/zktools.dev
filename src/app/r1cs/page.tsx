9c92e448a4bb11639fbd36ceb0647b7d4db141fa
Hugo Masclet <hugo@lukso.io>
2024-05-25 13:37:46 +0200

    Remove "Infos" from header

--- src/app/r1cs/page.tsx
+++ src/app/r1cs/page.tsx
@@ -4,8 +4,11 @@ import R1csInfo from "@/components/R1csInfo";
 export default function R1CS() {
   return (
     <div>
-      <h1>R1CS Infos</h1>
-      <InfoBox title="Rank-1 Constraint Systems (R1CS)" description = "Method to represent computations as a system of constraints that must be satisfied by the input values. It is a form of constraint system that converts a computation into a series of polynomial equations. These equations must hold true for the zk-SNARK proof to be valid." />
+      <h1>R1CS</h1>
+      <InfoBox
+        title="Rank-1 Constraint Systems (R1CS)"
+        description="Method to represent computations as a system of constraints that must be satisfied by the input values. It is a form of constraint system that converts a computation into a series of polynomial equations. These equations must hold true for the zk-SNARK proof to be valid."
+      />
       <R1csInfo />
     </div>
   );
