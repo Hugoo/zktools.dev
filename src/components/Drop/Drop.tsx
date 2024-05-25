c3ec953a1d75519bef8f7fcef71396ab63e43f39
Hugo Masclet <hugo@lukso.io>
2024-05-25 13:11:13 +0200

    Remove dark mode

--- src/components/Drop/Drop.tsx
+++ src/components/Drop/Drop.tsx
@@ -57,7 +57,7 @@ const Drop: React.FC<Props> = ({ onDrop }) => {
     >
       <label
         htmlFor="dropzone-file"
-        className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100"
+        className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
       >
         <div className="flex flex-col items-center justify-center pt-5 pb-6">
           {dropZoneContent()}
