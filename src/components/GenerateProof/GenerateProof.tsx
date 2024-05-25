2fd16c5a054318f8eb8e38756accf5fb76c32ebc
Hugo Masclet <hugo@lukso.io>
2024-05-25 18:28:30 +0200

    Add input for input value

--- src/components/GenerateProof/GenerateProof.tsx
+++ src/components/GenerateProof/GenerateProof.tsx
@@ -4,11 +4,12 @@ import { useEffect, useState } from "react";
 
 const GenerateProof: React.FC = () => {
   const [proof, setProof] = useState({});
+  const [inputValue, setInputValue] = useState({ x: 3, y: 11 });
 
   const generateProof = async () => {
     // @ts-ignore
     const { proof, publicSignals } = await snarkjs.groth16.fullProve(
-      { x: 3, y: 11 },
+      inputValue,
       "/multiply.wasm",
       "/circuit_0000.zkey"
     );
@@ -18,11 +19,19 @@ const GenerateProof: React.FC = () => {
 
   useEffect(() => {
     generateProof();
-  }, []);
+  }, [inputValue]);
 
   return (
     <div>
       <h2>Generate proofs</h2>
+      <h3>Input</h3>
+      <textarea
+        onChange={(e) => {
+          setInputValue(JSON.parse(e.target.value));
+        }}
+      >
+        {JSON.stringify(inputValue, null, 2)}
+      </textarea>
 
       <h3>Proof</h3>
       <pre>{JSON.stringify(proof, null, 2)}</pre>
