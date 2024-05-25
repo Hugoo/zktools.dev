75915fcff95de1fc2ea16fbc16df614fb6b940e3
omaliszewska <o.maliszewska97@gmail.com>
2024-05-25 18:20:02 +0200

    Add verification form and description to generate proof page

--- src/app/proof/generate/page.tsx
+++ src/app/proof/generate/page.tsx
@@ -1,11 +1,12 @@
 import InfoBox from "@/components/InfoBox";
+import VerifyForm from "@/components/VerifyForm";
 
 export default function Verify() {
   return (
     <div>
       <h1>Generate Proof</h1>
-      <InfoBox title="xx" description="xx" />
-
+      <InfoBox title="Generate zero-knowledge proof with chosen proving system." description="The function takes the proving key, the private witness and a public value as inputs." />
+      <VerifyForm />
       <div>Resources: XX</div>
     </div>
   );
