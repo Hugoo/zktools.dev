d80dc0a677789d4449afb62ddfa9ab3e5ebe82d7
omaliszewska <o.maliszewska97@gmail.com>
2024-05-26 09:55:01 +0200

    Add user stories

--- src/components/IconSection/IconSection.tsx
+++ src/components/IconSection/IconSection.tsx
@@ -3,89 +3,40 @@ const IconSection: React.FC = () => {
     <div>
       <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
         <div className="grid sm:grid-cols-2 lg:grid-cols-4 items-center gap-12">
-          <div>
-            <div className="relative flex justify-center items-center size-12 bg-white rounded-xl before:absolute before:-inset-px before:-z-[1] before:bg-gradient-to-br before:from-blue-600 before:via-transparent before:to-violet-600 before:rounded-xl">
-              <svg
-                className="flex-shrink-0 size-6 text-blue-600"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <rect width="10" height="14" x="3" y="8" rx="2" />
-                <path d="M5 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v16a2 2 0 0 1-2 2h-2.4" />
-                <path d="M8 18h.01" />
-              </svg>
+          <div className="text-center">
+            {" "}
+            <div className="relative flex justify-center items-center size-24 bg-white rounded-xl before:absolute before:-inset-px before:-z-[1] before:bg-gradient-to-br before:from-blue-600 before:via-transparent before:to-violet-600 before:rounded-xl">
+              🐼
             </div>
             <div className="mt-5">
               <h3 className="text-lg font-semibold text-gray-800">
-                Responsive
+                Panda Pazu
               </h3>
               <p className="mt-1 text-gray-600">
-                Responsive, and mobile-first project on the web
+                Panda Pazu loves to shop online for bamboo goodies, but she's
+                worried about sharing her credit card details with every store.
+                With Zero-Knowledge Proofs, Pazu can prove to the online store
+                that she has enough funds to make a purchase without revealing
+                her actual credit card number or bank balance.
               </p>
             </div>
           </div>
 
-          <div>
-            <div className="relative flex justify-center items-center size-12 bg-white rounded-xl before:absolute before:-inset-px before:-z-[1] before:bg-gradient-to-br before:from-blue-600 before:via-transparent before:to-violet-600 before:rounded-xl">
-              <svg
-                className="flex-shrink-0 size-6 text-blue-600"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <path d="M20 7h-9" />
-                <path d="M14 17H5" />
-                <circle cx="17" cy="17" r="3" />
-                <circle cx="7" cy="7" r="3" />
-              </svg>
+          <div className="text-center">
+            {" "}
+            <div className="relative flex justify-center items-center size-24 bg-white rounded-xl before:absolute before:-inset-px before:-z-[1] before:bg-gradient-to-br before:from-blue-600 before:via-transparent before:to-violet-600 before:rounded-xl">
+              🐨
             </div>
             <div className="mt-5">
               <h3 className="text-lg font-semibold text-gray-800">
-                Customizable
+                Koala Koro
               </h3>
               <p className="mt-1 text-gray-600">
-                Components are easily customized and extendable
-              </p>
-            </div>
-          </div>
-
-          <div>
-            <div className="relative flex justify-center items-center size-12 bg-white rounded-xl before:absolute before:-inset-px before:-z-[1] before:bg-gradient-to-br before:from-blue-600 before:via-transparent before:to-violet-600 before:rounded-xl">
-              <svg
-                className="flex-shrink-0 size-6 text-blue-600"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z" />
-                <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z" />
-              </svg>
-            </div>
-            <div className="mt-5">
-              <h3 className="text-lg font-semibold text-gray-800">
-                Documentation
-              </h3>
-              <p className="mt-1 text-gray-600">
-                Every component and plugin is well documented
+                Koala Koro is planning a treasure hunt with his friends. They
+                want to ensure that each member has found a specific clue
+                without disclosing where they found it. Using Zero-Knowledge
+                Proofs, they can verify that everyone has their designated clue
+                without revealing the exact location where they discovered it.
               </p>
             </div>
           </div>
